12367115757e02cfee8b53ca9c60c4d58e12cadf
Rodrigo Rocha <rodrigorochaed1@gmail.com>
2026-07-18 14:30:29 -0300

    feat: routes for booking

--- backend/src/routes/reservasRoutes.js
+++ backend/src/routes/reservasRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import controller from '../controllers/reservasController.js'; // Ajuste o caminho se necessário
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.status(501).json({ status: 'Not implemented yet' });
-});
+router.post('/', controller.create);
+router.get('/', controller.getAll);
+router.get('/quadra/:quadraId', controller.getByQuadra);
+router.get('/:id', controller.getById);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.delete);
 
 export default router;
