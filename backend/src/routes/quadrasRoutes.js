58146c7414a29fc81bfce5270f02d716c8d20cf6
Arthur Vinicius Carneiro Nunes <123214565+ArthurViniNunes@users.noreply.github.com>
2026-07-21 02:11:55 +0000

    feat: Add Async handler

--- backend/src/routes/quadrasRoutes.js
+++ backend/src/routes/quadrasRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import controller from '../controllers/quadrasController.js';
+import asyncHandler from '../middlewares/asyncHandler.js';
 
 const router = express.Router();
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
+router.get('/', asyncHandler(controller.getAll));
+router.get('/:id', asyncHandler(controller.getById));
 
 export default router;
