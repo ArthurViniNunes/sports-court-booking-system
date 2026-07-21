982b76457a6226ca298f73b0033093a4499ca665
Arthur Vinicius Carneiro Nunes <123214565+ArthurViniNunes@users.noreply.github.com>
2026-07-21 02:21:57 +0000

    Feat: Add async middleware to Reservas

--- backend/src/routes/reservasRoutes.js
+++ backend/src/routes/reservasRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import controller from '../controllers/reservasController.js';
+import asyncHandler from '../middlewares/asyncHandler.js'
 
 const router = express.Router();
 
-router.post('/', controller.create);
-router.get('/', controller.getAll);
-router.get('/quadra/:quadraId', controller.getByQuadra);
-router.get('/:id', controller.getById);
-router.put('/:id', controller.update);
-router.delete('/:id', controller.delete);
+router.post('/', asyncHandler(controller.create));
+router.get('/', asyncHandler(controller.getAll));
+router.get('/quadra/:quadraId', asyncHandler(controller.getByQuadra));
+router.get('/:id', asyncHandler(controller.getById));
+router.put('/:id', asyncHandler(controller.update));
+router.delete('/:id', asyncHandler(controller.delete));
 
 export default router;
