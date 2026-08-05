510149fbe13119e7d648dc23ed2b0cbc71888a19
Rodrigo Rocha <rodrigorochaed1@gmail.com>
2026-08-05 20:26:47 -0300

    feat: adding mui in frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import { Button, Container, Stack } from '@mui/material'
 
 function Home() {
 
   return (
     <>
-      <div>Hello world!</div>
+      <Container maxWidth="md" sx={{ py: 4 }}>
+        <Stack spacing={2}>
+          
+          <Button variant="contained">Hello World!</Button>
+        </Stack>
+      </Container>
     </>
   )
 }
