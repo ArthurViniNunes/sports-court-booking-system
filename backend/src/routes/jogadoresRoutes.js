9189b6dc4c73caa3069b027894344069a7167373
Rodrigo Rocha <rodrigorochaed1@gmail.com>
2026-08-10 00:19:11 -0300

    feat: adding auth middleware in routes

--- backend/src/routes/jogadoresRoutes.js
+++ backend/src/routes/jogadoresRoutes.js
@@ -1,15 +1,15 @@
 import express from 'express';
 import controller from '../controllers/jogadoresController.js';
 import wrapController from '../utils/wrapController.js';
+import { requireAuth, requireAdmin } from '../middlewares/authMiddleware.js';
 
 const c = wrapController(controller);
 
 const router = express.Router();
 
-router.post('/', c.create);
-router.get('/', c.getAll);
-router.get('/:id', c.getById);
-router.put('/:id', c.update);
-router.delete('/:id', c.delete);
+router.get('/', requireAuth, requireAdmin, c.getAll);
+router.get('/:id', requireAuth, c.getById);
+router.put('/:id', requireAuth, c.update);
+router.delete('/:id', requireAuth, c.delete);
 
 export default router;
--- backend/src/routes/quadrasRoutes.js
+++ backend/src/routes/quadrasRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import controller from '../controllers/quadrasController.js';
 import wrapController from '../utils/wrapController.js';
+import { requireAuth, requireAdmin } from '../middlewares/authMiddleware.js';
 
 const c = wrapController(controller);
 
 const router = express.Router();
 
-router.post('/', c.create);
-router.get('/', c.getAll);
-router.get('/:id', c.getById);
-router.put('/:id', c.update);
-router.delete('/:id', c.delete);
+router.post('/', requireAuth, requireAdmin, c.create);
+router.get('/', requireAuth, c.getAll);
+router.get('/:id', requireAuth, c.getById);
+router.put('/:id', requireAuth, c.update);
+router.delete('/:id', requireAuth, requireAdmin, c.delete);
 
 export default router;
